dd7c674eae140637084ac23f2c0331f7094656d2
Horacio G. de Oro <hgdeoro@gmail.com>
2013-05-30 20:59:46 -0300

    Add logout and a base template for the htmls

--- lumina/urls.py
+++ lumina/urls.py
@@ -15,4 +15,7 @@ urlpatterns = patterns('',
     #
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {
+        'next_page': '/',
+    }, name='logout', ),
 )
