0f7e5aef98486e58d198d4cc3e918df655abe94b
Horacio G. de Oro <hgdeoro@gmail.com>
2013-06-03 01:41:17 -0300

    Add middleware to report exceptions

---
+++ lumina/middleware.py
@@ -0,0 +1,26 @@
+'''
+Created on Jun 3, 2013
+
+@author: Horacio G. de Oro
+'''
+
+import logging
+import traceback
+import sys
+
+
+class LoggingMiddleware(object):
+    """
+    Middleware that logs exceptions.
+    """
+    _middleware_logger = logging.getLogger('LoggingMiddleware')
+
+    def process_exception(self, request, exception):
+        try:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            out = traceback.format_exception(exc_type,
+                exc_value, exc_traceback)
+            self._middleware_logger.error("".join(out))
+        except:
+            self._middleware_logger.exception(
+                "Excepcion detected while trying to report an exception. ")
