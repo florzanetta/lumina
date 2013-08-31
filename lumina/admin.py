3c6e43fc1896bfecbd27bc8a7b69332190b66ca4
Horacio G. de Oro <hgdeoro@gmail.com>
2013-08-31 15:11:04 -0300

    Refactor: FIX admin

--- lumina/admin.py
+++ lumina/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from lumina.models import Image, Album, SharedAlbum, \
-    ImageSelection, LuminaUser
 
+from lumina.models import LuminaUser, Image, Session, SharedSessionByEmail, \
+    ImageSelection, Studio, Customer
+
+admin.site.register(Customer)
 admin.site.register(Image)
-admin.site.register(Album)
-admin.site.register(SharedAlbum)
-admin.site.register(LuminaUser)
 admin.site.register(ImageSelection)
+admin.site.register(LuminaUser)
+admin.site.register(Session)
+admin.site.register(SharedSessionByEmail)
+admin.site.register(Studio)
