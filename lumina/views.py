d8a4b3e0690bf4ab98f85e6cddc44324c7c6a9a2
Horacio G. de Oro <hgdeoro@gmail.com>
2013-08-31 18:36:11 -0300

    Add label for submit

--- lumina/views.py
+++ lumina/views.py
@@ -475,7 +475,7 @@ class SessionUpdateView(UpdateView):
     # https://docs.djangoproject.com/en/1.5/ref/class-based-views/generic-editing/#updateview
     model = Session
     form_class = SessionUpdateForm
-    template_name = 'lumina/base_update_form.html'
+    template_name = 'lumina/base_create_update_form.html'
 
     def get_form(self, form_class):
         form = super(SessionUpdateView, self).get_form(form_class)
@@ -493,6 +493,7 @@ class SessionUpdateView(UpdateView):
     def get_context_data(self, **kwargs):
         context = super(SessionUpdateView, self).get_context_data(**kwargs)
         context['title'] = "Actualizar sesión"
+        context['submit_label'] = "Actualizar"
         return context
 
 
