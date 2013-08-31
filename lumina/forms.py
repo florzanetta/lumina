4e4249014d66e57f56e9bc73aeb782a116824097
Horacio G. de Oro <hgdeoro@gmail.com>
2013-08-31 17:56:41 -0300

    iCommented out code to be refactored

--- lumina/forms.py
+++ lumina/forms.py
@@ -7,120 +7,118 @@ Created on Jun 1, 2013
 from django import forms
 from django.forms.widgets import CheckboxSelectMultiple
 
-from lumina.models import Image, Album, SharedAlbum, ImageSelection, LuminaUser
+
+#===============================================================================
+# SharedAlbum
+#===============================================================================
+
+# class SharedAlbumCreateForm(forms.ModelForm):
+# 
+#     class Meta:
+#         model = SharedAlbum
+#         exclude = ('user', 'random_hash',)
 
 
 #===============================================================================
 # SharedAlbum
 #===============================================================================
 
-class SharedAlbumCreateForm(forms.ModelForm):
-
-    class Meta:
-        model = SharedAlbum
-        exclude = ('user', 'random_hash',)
-
-
-#===============================================================================
-# SharedAlbum
-#===============================================================================
-
-class ImageSelectionCreateForm(forms.ModelForm):
-
-    def clean_image_quantity(self):
-        data = self.cleaned_data['image_quantity']
-        if data <= 0:
-            raise(forms.ValidationError("La cantidad de imagenes debe ser mayor a 0"))
-
-        # Always return the cleaned data, whether you have changed it or not.
-        return data
-
-    class Meta:
-        model = ImageSelection
-        exclude = ('user', 'status', 'selected_images')
+# class ImageSelectionCreateForm(forms.ModelForm):
+# 
+#     def clean_image_quantity(self):
+#         data = self.cleaned_data['image_quantity']
+#         if data <= 0:
+#             raise(forms.ValidationError("La cantidad de imagenes debe ser mayor a 0"))
+# 
+#         # Always return the cleaned data, whether you have changed it or not.
+#         return data
+# 
+#     class Meta:
+#         model = ImageSelection
+#         exclude = ('user', 'status', 'selected_images')
 
 
 #===============================================================================
 # Album
 #===============================================================================
 
-class AlbumCreateForm(forms.ModelForm):
-
-    class Meta:
-        model = Album
-        exclude = ('user',)
-
-
-class AlbumUpdateForm(forms.ModelForm):
-
-    class Meta:
-        model = Album
-        exclude = ('user',)
-        widgets = {
-            'shared_with': CheckboxSelectMultiple(),
-        }
+# class AlbumCreateForm(forms.ModelForm):
+# 
+#     class Meta:
+#         model = Album
+#         exclude = ('user',)
+# 
+# 
+# class AlbumUpdateForm(forms.ModelForm):
+# 
+#     class Meta:
+#         model = Album
+#         exclude = ('user',)
+#         widgets = {
+#             'shared_with': CheckboxSelectMultiple(),
+#         }
 
 
 #===============================================================================
 # Image
 #===============================================================================
 
-class ImageCreateForm(forms.ModelForm):
-
-    class Meta:
-        model = Image
-        exclude = ('user', 'size', 'original_filename', 'content_type',)
-
-
-class ImageUpdateForm(forms.ModelForm):
-
-    class Meta:
-        model = Image
-        exclude = ('user', 'image', 'size', 'original_filename', 'content_type',)
+# class ImageCreateForm(forms.ModelForm):
+# 
+#     class Meta:
+#         model = Image
+#         exclude = ('user', 'size', 'original_filename', 'content_type',)
+# 
+# 
+# class ImageUpdateForm(forms.ModelForm):
+# 
+#     class Meta:
+#         model = Image
+#         exclude = ('user', 'image', 'size', 'original_filename', 'content_type',)
 
 
 #===============================================================================
 # Customer
 #===============================================================================
 
-class CustomerCreateForm(forms.ModelForm):
-    password1 = forms.CharField(
-        max_length=20, required=True, widget=forms.PasswordInput(), label=u'Contrasena')
-    password2 = forms.CharField(
-        max_length=20, required=True, widget=forms.PasswordInput(), label=u'Contrasena (otra vez)')
-
-    class Meta:
-        model = LuminaUser
-        fields = (
-            'username', 'first_name', 'last_name', 'email', 'is_active', 'password1', 'password2'
-        )
-
-    def clean(self):
-        super(CustomerCreateForm, self).clean()
-        password1 = self.cleaned_data.get('password1')
-        password2 = self.cleaned_data.get('password2')
-        if password1 != password2:
-            raise forms.ValidationError('Los passwords no concuerdan')
-        return self.cleaned_data
-
-
-class CustomerUpdateForm(forms.ModelForm):
-    password1 = forms.CharField(
-        max_length=20, required=False, widget=forms.PasswordInput(), label=u'Contrasena')
-    password2 = forms.CharField(
-        max_length=20, required=False, widget=forms.PasswordInput(),
-        label=u'Contrasena (otra vez)')
-
-    class Meta:
-        model = LuminaUser
-        fields = (
-            'first_name', 'last_name', 'email', 'is_active', 'password1', 'password2'
-        )
-
-    def clean(self):
-        super(CustomerUpdateForm, self).clean()
-        password1 = self.cleaned_data.get('password1')
-        password2 = self.cleaned_data.get('password2')
-        if password1 != password2:
-            raise forms.ValidationError('Los passwords no concuerdan')
-        return self.cleaned_data
+# class CustomerCreateForm(forms.ModelForm):
+#     password1 = forms.CharField(
+#         max_length=20, required=True, widget=forms.PasswordInput(), label=u'Contrasena')
+#     password2 = forms.CharField(
+#         max_length=20, required=True, widget=forms.PasswordInput(), label=u'Contrasena (otra vez)')
+# 
+#     class Meta:
+#         model = LuminaUser
+#         fields = (
+#             'username', 'first_name', 'last_name', 'email', 'is_active', 'password1', 'password2'
+#         )
+# 
+#     def clean(self):
+#         super(CustomerCreateForm, self).clean()
+#         password1 = self.cleaned_data.get('password1')
+#         password2 = self.cleaned_data.get('password2')
+#         if password1 != password2:
+#             raise forms.ValidationError('Los passwords no concuerdan')
+#         return self.cleaned_data
+# 
+# 
+# class CustomerUpdateForm(forms.ModelForm):
+#     password1 = forms.CharField(
+#         max_length=20, required=False, widget=forms.PasswordInput(), label=u'Contrasena')
+#     password2 = forms.CharField(
+#         max_length=20, required=False, widget=forms.PasswordInput(),
+#         label=u'Contrasena (otra vez)')
+# 
+#     class Meta:
+#         model = LuminaUser
+#         fields = (
+#             'first_name', 'last_name', 'email', 'is_active', 'password1', 'password2'
+#         )
+# 
+#     def clean(self):
+#         super(CustomerUpdateForm, self).clean()
+#         password1 = self.cleaned_data.get('password1')
+#         password2 = self.cleaned_data.get('password2')
+#         if password1 != password2:
+#             raise forms.ValidationError('Los passwords no concuerdan')
+#         return self.cleaned_data
