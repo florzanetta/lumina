7198994e3198b4cb40b927a5de3266fc31ebf339
Horacio G. de Oro <hgdeoro@gmail.com>
2013-10-21 22:00:48 -0300

    First basic implementation of report #1

--- lumina/views.py
+++ lumina/views.py
@@ -9,8 +9,10 @@ import decimal
 import zipfile
 import pygal
 import random
+import pprint
 
 from StringIO import StringIO
+from collections import defaultdict
 
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
@@ -27,6 +29,7 @@ from django.core.exceptions import SuspiciousOperation, ObjectDoesNotExist, \
     PermissionDenied
 from django.views.decorators.csrf import csrf_exempt
 from django.core.files.base import ContentFile
+from django.db import connection
 
 from lumina.pil_utils import generate_thumbnail
 from lumina.models import Session, Image, LuminaUser, Customer, \
@@ -127,18 +130,50 @@ def home(request):
 @login_required
 @cache_control(private=True)
 def view_report(request, report_id):
+    assert request.user.is_photographer()
     report_id = int(report_id)
     ctx = {}
     if report_id == 1:
         ctx['report_title'] = 'Costo (hs) vs Monto cobtrado ($) por tipo de cliente'
-        chart = pygal.XY(#@UndefinedVariable
+        chart = pygal.XY(# @UndefinedVariable
             stroke=False, legend_at_bottom=True, x_title="Horas", y_title="$")
         chart.title = ctx['report_title']
-        chart.add(
-            'Particular (eventos)',
-            [ (random.randint(15, 40), random.randint(2000, 9000)) for _ in range(0, random.randint(5, 10))])
-        chart.add('Particular (otros)', [ (random.randint(5, 25), random.randint(1500, 4000)) for _ in range(0, random.randint(5, 10))])
-        chart.add('Agencia de publicidad', [ (random.randint(15, 90), random.randint(6000, 20000)) for _ in range(0, random.randint(5, 10))])
+
+        cursor = connection.cursor()
+        cursor.execute("SELECT ls.session_type_id,"
+            " lst.name AS session_type_name,"
+            " ls.worked_hours AS worked_hours,"
+            " lsq.cost AS orig_cost,"
+            " lsqa.cost AS selected_quote_alternative_cost"
+            " FROM lumina_session AS ls JOIN lumina_sessionquote AS lsq ON lsq.session_id = ls.id"
+            " JOIN lumina_sessiontype AS lst ON ls.session_type_id = lst.id"
+            " LEFT OUTER JOIN lumina_sessionquotealternative AS lsqa"
+            " ON lsq.accepted_quote_alternative_id = lsqa.id"
+            " WHERE ls.worked_hours > 0 AND ls.studio_id = %s", [request.user.studio.id])
+        desc = cursor.description
+        values_as_dict = [
+            dict(zip([col[0] for col in desc], row))
+            for row in cursor.fetchall()
+        ]
+
+        group_by_session_type = defaultdict(list)
+        for item in values_as_dict:
+            group_by_session_type[item['session_type_name']].append(item)
+
+        logger.info("group_by_session_type: %s", pprint.pformat(group_by_session_type))
+
+        for a_session_type, items in group_by_session_type.iteritems():
+            values = [[0,0]] # HACK! without this, charts with 1 value doesn't show up
+            for item in items:
+                # (horas, costo)
+                hours = float(item['worked_hours'])
+                cost = item['selected_quote_alternative_cost'] or item['orig_cost']
+                cost = float(cost)
+                values.append([hours, cost])
+                logger.info(" - value: %s, %s", hours, cost)
+            logger.info(" - Adding new serie %s with %s values", a_session_type, len(values))
+            chart.add(a_session_type, values)
+
         chart.print_values = False
         ctx['svg_chart'] = chart.render()
         ctx['show_form_1'] = True
