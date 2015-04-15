640b5e6bf639edcac0853759e70cd1c7a011f403
Horacio G. de Oro <hgdeoro@gmail.com>
2015-04-14 23:24:34 -0300

    Import autocomplete_light to avoid Warnings

--- manage.py
+++ manage.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 import os
 import sys
+import warnings
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lumina.settings")
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        import autocomplete_light
 
     from django.core.management import execute_from_command_line
 
