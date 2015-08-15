6620760d2b460e54bbd129d7ab7e3370c4a96c66
Horacio G. de Oro <hgdeoro@gmail.com>
2015-08-15 01:31:32 -0300

    Setup automatic reload of uWSGI

--- lumina/settings_uwsgi.py
+++ lumina/settings_uwsgi.py
@@ -6,3 +6,20 @@ DEBUG = False
 # Dump object with {% dump_objects %}
 #
 LUMINA_DUMP_OBJECTS = False
+
+
+try:
+    import uwsgi
+    from uwsgidecorators import timer
+    from django.utils import autoreload
+
+    @timer(3)
+    def change_code_gracefull_reload(sig):
+        if autoreload.code_changed():
+            uwsgi.reload()
+
+    print("AUTO-reloading of uWSGI activated")
+
+except:
+    import logging
+    logging.exception("NOT using auto-reloading")
