c56393ae4a6ee6ba722f36a847e29753563e74c9
Horacio G. de Oro <hgdeoro@gmail.com>
2015-09-13 22:55:37 -0300

    Refactor: move code to base form class

--- lumina/forms_reports.py
+++ lumina/forms_reports.py
@@ -9,8 +9,7 @@ from lumina import forms_utils
 logger = logging.getLogger(__name__)
 
 
-class CostVsChargedByCustomerReportForm(forms_utils.GenericForm):
-    FORM_TITLE = 'Costo (hs) vs Monto cobrado ($) por tipo de cliente'
+class _GenericDateRangeReportForm(forms_utils.GenericForm):
     SUBMIT_LABEL = 'Actualizar reporte'
     FIELDS = [
         'fecha_desde', 'fecha_hasta',
@@ -18,21 +17,32 @@ class CostVsChargedByCustomerReportForm(forms_utils.GenericForm):
 
     fecha_desde = forms.DateField(required=False, label='Fecha desde')
     fecha_hasta = forms.DateField(required=False, label='Fecha hasta')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        fecha_desde = cleaned_data.get("fecha_desde")
+        fecha_hasta = cleaned_data.get("fecha_hasta")
+
+        if fecha_desde and fecha_hasta:
+            if fecha_desde > fecha_hasta:
+                msg = "Fecha desde debe ser anterior a fecha hasta"
+                self.add_error('fecha_desde', msg)
+                self.add_error('fecha_hasta', msg)
+
+
+class CostVsChargedByCustomerReportForm(_GenericDateRangeReportForm):
+    FORM_TITLE = 'Costo (hs) vs Monto cobrado ($) por tipo de cliente'
+    FIELDS = _GenericDateRangeReportForm.FIELDS + []
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper.form_action = 'report_cost_vs_charged_by_customer_type'
 
 
-class ExtendedQuotesThroughTimeReportForm(forms_utils.GenericForm):
+class ExtendedQuotesThroughTimeReportForm(_GenericDateRangeReportForm):
     FORM_TITLE = 'Presupuestos expandidos (en el tiempo)'
-    SUBMIT_LABEL = 'Actualizar reporte'
-    FIELDS = [
-        'fecha_desde', 'fecha_hasta',
-    ]
+    FIELDS = _GenericDateRangeReportForm.FIELDS + ['session_type']
 
-    fecha_desde = forms.DateField(required=False, label='Fecha desde')
-    fecha_hasta = forms.DateField(required=False, label='Fecha hasta')
     session_type = forms.ChoiceField(required=False, label='Tipo de sesión')
 
     def __init__(self, *args, **kwargs):
@@ -40,15 +50,10 @@ class ExtendedQuotesThroughTimeReportForm(forms_utils.GenericForm):
         self.helper.form_action = 'report_extended_quotes_through_time'
 
 
-class ExtendedQuotesByCustomerReportForm(forms_utils.GenericForm):
+class ExtendedQuotesByCustomerReportForm(_GenericDateRangeReportForm):
     FORM_TITLE = 'Presupuestos expandidos (por cliente)'
-    SUBMIT_LABEL = 'Actualizar reporte'
-    FIELDS = [
-        'fecha_desde', 'fecha_hasta', 'session_type'
-    ]
+    FIELDS = _GenericDateRangeReportForm.FIELDS + ['session_type']
 
-    fecha_desde = forms.DateField(required=False, label='Fecha desde')
-    fecha_hasta = forms.DateField(required=False, label='Fecha hasta')
     session_type = forms.ChoiceField(required=False, label='Tipo de sesión')
 
     def __init__(self, *args, **kwargs):
@@ -56,15 +61,10 @@ class ExtendedQuotesByCustomerReportForm(forms_utils.GenericForm):
         self.helper.form_action = 'report_extended_quotes_by_customer'
 
 
-class IncomeByCustomerTypeReportForm(forms_utils.GenericForm):
+class IncomeByCustomerTypeReportForm(_GenericDateRangeReportForm):
     FORM_TITLE = 'Ingresos ($) por tipo de cliente'
-    SUBMIT_LABEL = 'Actualizar reporte'
-    FIELDS = [
-        'fecha_desde', 'fecha_hasta', 'session_type'
-    ]
+    FIELDS = _GenericDateRangeReportForm.FIELDS + ['session_type']
 
-    fecha_desde = forms.DateField(required=False, label='Fecha desde')
-    fecha_hasta = forms.DateField(required=False, label='Fecha hasta')
     session_type = forms.ChoiceField(required=False, label='Tipo de sesión')
 
     def __init__(self, *args, **kwargs):
