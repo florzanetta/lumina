84d88bfaf8143cc8017a9341e7d61167d1cdfc0a
Horacio G. de Oro <hgdeoro@gmail.com>
2015-10-10 13:12:16 -0300

    FIX LUM-195 Add success message

--- lumina/views_studio.py
+++ lumina/views_studio.py
@@ -3,6 +3,7 @@
 import logging
 
 from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from django.core.urlresolvers import reverse_lazy
 from django.views import generic
 
@@ -13,27 +14,16 @@ from lumina import forms_users
 logger = logging.getLogger(__name__)
 
 
-class StudioUpdateView(generic.UpdateView):
+class StudioUpdateView(SuccessMessageMixin,
+                       generic.UpdateView):
     model = models.Studio
     form_class = forms_studio.StudioUpdateForm
     template_name = 'lumina/base_create_update_crispy_form.html'
     success_url = reverse_lazy('home')
+    success_message = 'Los datos del estudio fotográfico fueron actualizados exitosamente'
 
     def get_object(self, queryset=None):
         return self.request.user.studio
-
-        # def form_valid(self, form):
-        #     ret = super().form_valid(form)
-        #
-        #     # Set the password
-        #     if form['password1'].value():
-        #         updated_user = models.LuminaUser.objects.get(pk=form.instance.id)
-        #         logger.warn("Changing password of user '%s'", updated_user.username)
-        #         updated_user.set_password(form['password1'].value())
-        #         updated_user.save()
-        #
-        #     messages.success(self.request, 'El usuario fue actualizado correctamente')
-        #     return ret
 
 
 class StudioCreateView(generic.CreateView):
