c47cf965446251b05eb21f131abb4c9c9b679c48
Horacio G. de Oro <hgdeoro@gmail.com>
2015-11-11 15:20:31 -0300

    Implement query for report

--- lumina/views_reports.py
+++ lumina/views_reports.py
@@ -96,84 +96,6 @@ def view_report_cost_vs_charged_by_customer_type(request):
 
 @login_required
 @cache_control(private=True)
-def view_extended_quotes_through_time(request):
-    assert request.user.is_photographer()
-
-    if request.method == 'GET':
-        form = forms_reports.ExtendedQuotesThroughTimeReportForm(user=request.user)
-    else:
-        form = forms_reports.ExtendedQuotesThroughTimeReportForm(request.POST,
-                                                                 user=request.user)
-
-    ctx = dict(form=form)
-
-    ctx['report_title'] = 'Presupuestos expandidos (en el tiempo)'
-    chart = pygal.StackedBar(legend_at_bottom=True,
-                             y_title="$",
-                             x_label_rotation=20,
-                             config=PYGAL_CONFIG)
-    chart.title = ctx['report_title']
-
-    cursor = connection.cursor()
-    # FIXME: use only accepted quotes! (not canceled, or rejected by customer)
-    cursor.execute(
-        "SELECT "
-        " lsq.created AS \"date_for_report\","
-        " lsq.cost AS \"orig_cost\","
-        " lsqa.cost AS \"selected_quote_alternative_cost\","
-        " c.name AS \"customer\""
-        " FROM lumina_session AS ls"
-        " JOIN lumina_sessionquote AS lsq ON lsq.session_id = ls.id"
-        " JOIN lumina_customer AS c ON ls.customer_id = c.id"
-        " LEFT OUTER JOIN lumina_sessionquotealternative AS lsqa"
-        "    ON lsq.accepted_quote_alternative_id = lsqa.id"
-        " WHERE ls.studio_id = %s", [request.user.studio.id])
-    desc = cursor.description
-    values_as_dict = [
-        dict(list(zip([col[0] for col in desc], row)))
-        for row in cursor.fetchall()
-    ]
-
-    # pprint.pprint(values_as_dict)
-
-    group_by_date = defaultdict(list)
-    for item in values_as_dict:
-        group_by_date[(item['date_for_report'].year,
-                       item['date_for_report'].month)].append(item)
-    dates = list(group_by_date.keys())
-    dates.sort()
-
-    logger.info("group_by_date: %s", pprint.pformat(group_by_date))
-
-    serie_cost = []
-    serie_alt_quote = []
-    labels = []
-
-    for year, month in dates:
-        acum_cost = 0.0
-        acum_alt_quote = 0.0
-        for item in group_by_date[(year, month)]:
-            acum_cost += float(item['orig_cost'])
-            if item['selected_quote_alternative_cost']:
-                acum_alt_quote += float(item['selected_quote_alternative_cost'])
-        labels.append("{}/{}".format(month, year))
-        serie_cost.append(acum_cost)
-        serie_alt_quote.append(acum_alt_quote)
-
-    chart.x_labels = labels
-    chart.add('Presupuesto original', serie_cost)
-    chart.add('Presupuesto expandido', serie_alt_quote)
-    chart.print_values = False
-    ctx['svg_chart'] = chart.render()
-    ctx['show_form_2'] = True
-
-    return render_to_response(
-        'lumina/reports/report_generic.html', ctx,
-        context_instance=RequestContext(request))
-
-
-@login_required
-@cache_control(private=True)
 def view_extended_quotes_by_customer(request):
     assert request.user.is_photographer()
 
@@ -316,6 +238,99 @@ def view_income_by_customer_type(request):
         chart.add(label, total_by_customer[customer_type])
 
     ctx['svg_chart'] = chart.render()
+
+    return render_to_response(
+        'lumina/reports/report_generic.html', ctx,
+        context_instance=RequestContext(request))
+
+
+@login_required
+@cache_control(private=True)
+def view_extended_quotes_through_time(request):
+    assert request.user.is_photographer()
+
+    ctx = {
+        'report_title': 'Presupuestos expandidos (en el tiempo)',
+    }
+
+    if request.method == 'GET':
+        form = forms_reports.ExtendedQuotesThroughTimeReportForm(user=request.user)
+        ctx['form'] = form
+
+        return render_to_response(
+            'lumina/reports/report_generic.html', ctx,
+            context_instance=RequestContext(request))
+
+    else:
+        form = forms_reports.ExtendedQuotesThroughTimeReportForm(request.POST, user=request.user)
+        ctx['form'] = form
+
+        if not form.is_valid():
+            return render_to_response(
+                'lumina/reports/report_generic.html', ctx,
+                context_instance=RequestContext(request))
+
+    cursor = connection.cursor()
+    cursor.execute("""
+    SELECT
+        sess_quo.created    AS "date_for_report",
+        sess_quo.cost       AS "orig_cost",
+        sess_quo_alt.cost   AS "selected_quote_alternative_cost",
+        cust.name           AS "customer"
+    FROM
+        lumina_sessionquote as sess_quo
+        JOIN lumina_customer AS cust
+            ON sess_quo.customer_id = cust.id
+        LEFT OUTER JOIN lumina_sessionquotealternative AS sess_quo_alt
+            ON sess_quo.accepted_quote_alternative_id = sess_quo_alt.id
+    WHERE
+        sess_quo.status = %s AND
+        sess_quo.studio_id = %s
+    """, [models.SessionQuote.STATUS_ACCEPTED,
+          request.user.studio.id])
+
+    desc = cursor.description
+    values_as_dict = [
+        dict(list(zip([col[0] for col in desc], row)))
+        for row in cursor.fetchall()
+    ]
+
+    group_by_date = defaultdict(list)
+    for item in values_as_dict:
+        group_by_date[(item['date_for_report'].year,
+                       item['date_for_report'].month)].append(item)
+    dates = list(group_by_date.keys())
+    dates.sort()
+
+    logger.info("group_by_date: %s", pprint.pformat(group_by_date))
+
+    serie_cost = []
+    serie_alt_quote = []
+    labels = []
+
+    for year, month in dates:
+        acum_cost = 0.0
+        acum_alt_quote = 0.0
+        for item in group_by_date[(year, month)]:
+            acum_cost += float(item['orig_cost'])
+            if item['selected_quote_alternative_cost']:
+                acum_alt_quote += float(item['selected_quote_alternative_cost'])
+        labels.append("{}/{}".format(month, year))
+        serie_cost.append(acum_cost)
+        serie_alt_quote.append(acum_alt_quote)
+
+    chart = pygal.StackedBar(legend_at_bottom=True,
+                             y_title="$",
+                             x_label_rotation=20,
+                             config=PYGAL_CONFIG)
+    chart.title = ctx['report_title']
+
+    chart.x_labels = labels
+    chart.add('Presupuesto original', serie_cost)
+    chart.add('Presupuesto expandido', serie_alt_quote)
+    chart.print_values = False
+    ctx['svg_chart'] = chart.render()
+    ctx['show_form_2'] = True
 
     return render_to_response(
         'lumina/reports/report_generic.html', ctx,
